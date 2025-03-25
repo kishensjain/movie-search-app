382d8e983f4ed1f2c0c2cb1b7758b1ea3cb367f5
Kishen S Jain <jain.skishen@gmail.com>
2025-03-25 11:31:17 +0530

    added searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import SearchBar from "./components/SearchBar"; // Import the SearchBar component
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (query) =>{
+    console.log(`Searching for ${query}`);
+    
+  }
 
   return (
-    <>
-    </>
-  )
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center p-6">
+      <h1 className="text-3xl font-bold mb-6">Movie Search App🎬</h1>
+      <SearchBar onSearch={handleSearch}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+// 1aaf93b1480cccac094826255d1424ea
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"; 
+import { FaSearch } from "react-icons/fa"; // Import search icon
+
+const SearchBar = ({onSearch}) => {
+  const [query, setQuery] = useState(""); // State to store user input
+
+  useEffect(() => {
+    if(!query) return;
+
+    const delayDebounce = setTimeout(()=>{
+      if (onSearch) onSearch(query);
+    }, 500);
+    return () => clearTimeout(delayDebounce)
+  }, [query]);
+
+  return (
+    
+    <div className="flex items-center gap-2 p-4 bg-gray-800 rounded-lg shadow-md">
+
+      {/* Search Input Field */}
+      <input
+        type="text"
+        placeholder="Search for a movie..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="bg-gray-700 text-white placeholder-gray-400 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 flex-1"
+      />
+
+      {/* Search Icon */}
+      <FaSearch className="text-white text-xl" />
+      
+    </div>
+    
+  );
+};
+
+export default SearchBar;

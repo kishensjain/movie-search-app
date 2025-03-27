01c28f348f5f228e65f0027a0584ef0d15cecfbd
Kishen S Jain <jain.skishen@gmail.com>
2025-03-27 11:40:42 +0530

    added movie details page

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ const MovieList = ({searchResults, hasSearched}) => {
     <div className = "grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
       {searchResults.length > 0 ? (
         searchResults.map((movie) => (
-          <Link to = {'/movie/${movie.id}'} key={movie.id}>
+          <Link to = {`/movie/${movie.id}`} key={movie.id}>
           <div
             key={movie.id}
             className="bg-gray-800 p-4 rounded-lg shadow-lg hover:scale-105 transition-transform">
